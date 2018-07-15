67d571bc440f70de97e2a0e099ca83c30ea5ccce
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-15 14:46:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mary"
+  end
+
+  get '/hometown' do
+    "My hometown is Aiken"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I Walk the Line"
+  end
+
 end
